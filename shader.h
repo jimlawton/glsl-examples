98c47872ff3c35cd90a38481efcd66d6737b9c60
Jim Lawton <jim.lawton@gmail.com>
2013-01-20 19:51:43 +0000

    Remove unneeded includes.

--- shader.h
+++ shader.h
@@ -1,7 +1,5 @@
 
 #include <GL/gl.h>
-#include <GL/glext.h>
-#include <GL/glut.h>
 
 // Compile a GLSL shader file, and return the shader object.
 GLuint compileShader(GLenum type, const char *filePath);
